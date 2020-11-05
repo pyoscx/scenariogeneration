8b1cb75086592549d6749e61716ea2958a6c8bd0
Huub Joosten <huub.joosten@navinfo.eu>
2020-11-05 12:51:59 +0100

    Updates for FileHeader attributes

--- pyoscx/utils.py
+++ pyoscx/utils.py
@@ -1114,7 +1114,7 @@ class FileHeader():
         """ returns the attributes as a dict of the FileHeader
 
         """
-        return {'decription':self.name,'author':self.author,'revMajor':'1','revMinor':'0','date':str(dt.datetime.now())}
+        return {'description':self.name,'author':self.author,'revMajor':'1','revMinor':'0','date':dt.datetime.now().isoformat()}
 
     def get_element(self):
         """ returns the elementTree of the FileHeader
