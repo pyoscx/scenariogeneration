3e1e5a999df4e625a429b04f852f2bd20ec060e6
Kaths, Jakob <Jakob.Kaths@vector.com>
2021-01-28 10:25:05 +0100

    Add repeated objects

--- pyodrx/objects.py
+++ pyodrx/objects.py
@@ -3,55 +3,6 @@ from .helpers import enum2str
 from .enumerations import ObjectType, Orientation, Dynamic 
 
 
-# =============================================================================
-# class Objects():
-#     """ creates the Lanes element of opendrive
-#     
-# 
-#         Attributes
-#         ----------
-#             lane_sections (list of LaneSection): a list of all lanesections
-# 
-#         Methods
-#         -------
-#             get_element(elementname)
-#                 Returns the full ElementTree of the class
-# 
-#             add_lanesection(lanesection)
-#                 adds a lane section to Lanes
-#     """
-#     def __init__(self):
-#         """ initalize Lanes
-# 
-#         """
-#         self.objects = []
-#     def add_object(self,road_object):
-#         """ creates the Lanes element of opendrive
-#     
-# 
-#         Parameters
-#         ----------
-#             lanesection (LaneSection): a LaneSection to add
-# 
-#             lanelink (LaneLinker): (optional) a LaneLink to add 
-# 
-#         """
-#         if not isinstance(road_object, list):
-#             self.objects.append(road_object)
-#         else:
-#             self.objects = self.objects+road_object
-# 
-#     def get_element(self):
-#         """ returns the elementTree of Lanes
-# 
-#         """
-#         element = ET.Element('objects')
-#         for road_object in self.objects:
-#             element.append(road_object.get_element())
-#         return element
-# =============================================================================
-
-
 class Object():
     """ creates an Object
 
@@ -99,7 +50,7 @@ class Object():
                 adds a new roadmark to the lane
 
     """
-    def __init__(self,object_type,object_id,s,t,height,object_subtype=None,dynamic=Dynamic.no,name='defaultName',zOffset=0,validLength=0,orientation=Orientation.none,hdg=0,pitch=0,roll=0,width=None,length=None,radius=None):
+    def __init__(self,s,t,height,object_id,dynamic=Dynamic.no,name='defaultName',zOffset=0,Type=ObjectType.none,subtype=None,validLength=0,orientation=Orientation.none,hdg=0,pitch=0,roll=0,width=None,length=None,radius=None):
         """ initalizes the Object
 
         Parameters
@@ -120,16 +71,16 @@ class Object():
                 Default: 'standard'
 
         """ 
-        self.object_type = object_type
-        self.object_id = object_id
         self.s = s
         self.t = t
         self.height = height
+        self.object_id = object_id
         
-        self.object_subtype = object_subtype
         self.dynamic = dynamic
         self.name = name
         self.zOffset = zOffset
+        self.Type = Type
+        self.subtype = subtype
         self.validLength = validLength
         self.orientation = orientation
         self.hdg = hdg
@@ -139,7 +90,10 @@ class Object():
         self.length = length
         self.radius = radius
         
-        #change default line values for broken lines 
+        self._repeats = []
+        
+        
+        #check if width/length combination or radius was provided and ensure working defaults 
         if radius is not None and (width is not None or length is not None):
             print ("Object with id",self.object_id,"was provided with radius, width and/or length. Provide either radius or width and length. Using radius as fallback.")
             self.width = None
@@ -155,18 +109,79 @@ class Object():
             self.radius = 0
         else:
             pass
+        
+    def repeat(self,repeatLength,repeatDistance,sStart=None,tStart=None,tEnd=None,heightStart=None,heightEnd=None,zOffsetStart=None,zOffsetEnd=None,widthStart=None,widthEnd=None,lengthStart=None,lengthEnd=None,radiusStart=None,radiusEnd=None):
+        
+        self._repeats.append({})
+
+        self._repeats[-1]['length']=str(repeatLength)
+        self._repeats[-1]['distance']=str(repeatDistance)
+        
+        #ensuring that all attributes that are required according to OpenDRIVE 1.6 are filled - for convenience the ones of the parent object are used
+        #if not provided specifically  
+        
+        if sStart == None:
+            self._repeats[-1]['s']=str(self.s)
+            print ("Info: no s-coordinate specified for repeating object with id",self.object_id ,", using s-coordinate of original object instead.")
+        else:
+            self._repeats[-1]['s']=str(sStart)           
+        if tStart == None:
+            self._repeats[-1]['tStart']=str(self.t)
+            print ("Info: no starting t-coordinate specified for repeating object with id",self.object_id ,", using t-coordinate of original object instead.")
+        else:
+            self._repeats[-1]['tStart']=str(tStart)
+        if tEnd == None:
+            self._repeats[-1]['tEnd']=str(self.t)
+            print ("Info: no ending t-coordinate specified for repeating object with id",self.object_id ,", using t-coordinate of original object instead.")
+        else:
+            self._repeats[-1]['tEnd']=str(tEnd)
+        if heightStart == None:
+            self._repeats[-1]['heightStart']=str(self.height)
+            print ("Info: no starting height specified for repeating object with id",self.object_id ,", using height of original object instead.")
+        else:
+            self._repeats[-1]['heightStart']=str(heightStart)
+        if heightEnd == None:
+            self._repeats[-1]['heightEnd']=str(self.height)
+            print ("Info: no ending height specified for repeating object with id",self.object_id ,", using height of original object instead.")
+        else:
+            self._repeats[-1]['heightEnd']=str(heightEnd)
+        if zOffsetStart == None:
+            self._repeats[-1]['zOffsetStart']=str(self.zOffset)
+            print ("Info: no starting zOffset specified for repeating object with id",self.object_id ,", using zOffset of original object instead.")
+        else:
+            self._repeats[-1]['zOffsetStart']=str(zOffsetStart)
+        if zOffsetEnd == None:
+            self._repeats[-1]['zOffsetEnd']=str(self.zOffset)
+            print ("Info: no ending zOffset specified for repeating object with id",self.object_id ,", using zOffset of original object instead.")
+        else:
+            self._repeats[-1]['zOffsetEnd']=str(zOffsetEnd)
+        
+        #attributes below are optional according to OpenDRIVE 1.6 - no further checks as these values overrule the ones of parent object
+        #and fallbacks might be implemented differently by different simulators
+        if widthStart is not None:
+             self._repeats[-1]['widthStart']=str(widthStart)
+        if widthEnd is not None:
+             self._repeats[-1]['widthEnd']=str(widthEnd)
+        if lengthStart is not None:
+             self._repeats[-1]['lengthStart']=str(lengthStart)
+        if lengthEnd is not None:
+             self._repeats[-1]['lengthEnd']=str(lengthEnd)
+        if radiusStart is not None:
+             self._repeats[-1]['radiusStart']=str(radiusStart)
+        if radiusEnd is not None:
+             self._repeats[-1]['radiusEnd']=str(radiusEnd)           
             
     def get_attributes(self):
-        """ returns the attributes of the Lane as a dict
+        """ returns the attributes of the Object as a dict
 
         """
         retdict = {}
-        retdict['type'] = enum2str(self.object_type)
+        retdict['type'] = enum2str(self.Type)
         retdict['id'] = str(self.object_id)
         retdict['s'] = str(self.s)
         retdict['t'] = str(self.t)
         retdict['height'] = str(self.height)
-        retdict['subtype'] = str(self.object_subtype)
+        retdict['subtype'] = str(self.subtype)
         retdict['dynamic'] = enum2str(self.dynamic)
         retdict['name'] = str(self.name)
         retdict['zOffset'] = str(self.zOffset)
@@ -195,4 +210,11 @@ class Object():
 
         """
         element = ET.Element('object',attrib=self.get_attributes())
+        for _repeat in self._repeats:
+            ET.SubElement(element,'repeat', attrib=_repeat)
+
         return element
+    
+#TODO Adjust all comments
+#TODO Allow repeated
+#TODO Generate tests
