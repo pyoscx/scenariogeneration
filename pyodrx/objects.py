6e2bbc6cffa955a4115b6cbd46c5f4ca20ea30db
Kaths, Jakob <Jakob.Kaths@vector.com>
2021-01-28 16:18:27 +0100

    Use common base class for signals and objects

--- pyodrx/objects.py
+++ pyodrx/objects.py
@@ -2,97 +2,243 @@ import xml.etree.ElementTree as ET
 from .helpers import enum2str
 from .enumerations import ObjectType, Orientation, Dynamic 
 
-
-class Object():
-    """ creates an Object
-
-        Parameters
-        ----------
-            marking_type (RoadMarkType): the type of marking
-
-            width (float): with of the line
-
-            length (float): length of the line
-                Default: 0
-            toffset (float): offset in t
-                Default: 0
-            soffset (float): offset in s
-                Default: 0
-            rule (MarkRule): mark rule (optional)
-
-            color (RoadMarkColor): color of line (optional)
+class _SignalObjectCommon():
+    """ creates a common basis for Signal and Object shall not be instantiated directly
 
         Attributes
         ----------
-            marking_type (str): the type of marking
+            s (float): s-coordinate of Signal / Object
 
-            width (float): with of the line
+            t (float): t-coordinate of Signal / Object
 
-            length (float): length of the line
-                Default: 0
-            toffset (float): offset in t
-                Default: 0
-            soffset (float): offset in s
-                Default: 0
-            rule (MarkRule): mark rule (optional)
+            height (float): height of Signal / Object
 
-            color (RoadMarkColor): color of line (optional)
+            object_id (string): id of Signal / Object
+
+            type (ObjectType or string): type of the Signal (typically string) / Object (typically enum ObjectType)
+
+            subtype (string): subtype for further specification of Signal / Object
+
+            dynamic (Dynamic): specifies if Signal / Object is static (road sign) or dynamic (traffic light)
+            
+            name (string): name for identification of Signal / Object
+            
+            zOffset (float): vertical offset of Signal / Object with respect to centerline
+            
+            orientation (Orientation): orientation of Signal / Object with respect to road
+            
+            pitch (float): pitch angle (rad) of Signal / Object relative to the inertial system (xy-plane)
+            
+            roll (float): roll angle (rad) of Signal / Object after applying pitch, relative to the inertial system (x’’y’’-plane) 
+            
+            width (float): width of the Signal / Object
+
 
         Methods
         -------
-            get_element(elementname)
-                Returns the full ElementTree of the class
-
-            get_attributes()
+            get_common_attributes()
                 Returns a dictionary of all attributes of FileHeader
-
-            add_roadmark(roadmark)
-                adds a new roadmark to the lane
-
-    """
-    def __init__(self,s,t,height,object_id,dynamic=Dynamic.no,name='defaultName',zOffset=0,Type=ObjectType.none,subtype=None,validLength=0,orientation=Orientation.none,hdg=0,pitch=0,roll=0,width=None,length=None,radius=None):
-        """ initalizes the Object
+        """
+        
+    def __init__(self,s,t,height,object_id,Type,subtype,dynamic,name,zOffset,orientation,pitch,roll,width):
+        """ initalizes common attributes for Signal and Object
 
         Parameters
         ----------
-            marking_type (str): the type of marking
+            s (float): s-coordinate of Signal / Object
 
-            width (float): with of the line
+            t (float): t-coordinate of Signal / Object
 
-            length (float): length of the line
-                Default: 0
-            toffset (float): offset in t
-                Default: 0
-            soffset (float): offset in s
-                Default: 0
-            rule (MarkRule): mark rule (optional)
+            height (float): height of Signal / Object
 
-            color (RoadMarkColor): color of line
-                Default: 'standard'
+            object_id (string): id of Signal / Object
+
+            type (ObjectType or string): type of the Signal (typically string) / Object (typically enum ObjectType)
+
+            subtype (string): subtype for further specification of Signal / Object
+
+            dynamic (Dynamic): specifies if Signal / Object is static (road sign) or dynamic (traffic light)
+            
+            name (string): name for identification of Signal / Object
+            
+            zOffset (float): vertical offset of Signal / Object with respect to centerline
+            
+            orientation (Orientation): orientation of Signal / Object with respect to road
+            
+            pitch (float): pitch angle (rad) of Signal / Object relative to the inertial system (xy-plane)
+            
+            roll (float): roll angle (rad) of Signal / Object after applying pitch, relative to the inertial system (x’’y’’-plane)
+            
+            width (float): width of the Signal / Object
 
         """ 
         self.s = s
         self.t = t
         self.height = height
         self.object_id = object_id
+        self.Type = Type
         
         self.dynamic = dynamic
         self.name = name
         self.zOffset = zOffset
-        self.Type = Type
         self.subtype = subtype
-        self.validLength = validLength
         self.orientation = orientation
-        self.hdg = hdg
         self.pitch = pitch
         self.roll = roll
         self.width = width
+        
+
+        
+    def get_common_attributes(self):
+            """ returns common attributes of Signal and Object as a dict
+    
+            """
+            retdict = {}
+            retdict['id'] = str(self.object_id)
+            retdict['s'] = str(self.s)
+            retdict['t'] = str(self.t)
+            retdict['height'] = str(self.height)
+            retdict['subtype'] = str(self.subtype)
+            retdict['dynamic'] = enum2str(self.dynamic)
+            retdict['name'] = str(self.name)
+            retdict['zOffset'] = str(self.zOffset)
+            retdict['pitch'] = str(self.pitch)
+            retdict['roll'] = str(self.roll)
+            retdict['width'] = str(self.width)            
+            
+            if (isinstance(self.Type, ObjectType)):
+                retdict['type'] = enum2str(self.Type)
+            else:
+                retdict['type'] = str(self.Type)               
+         
+            if self.orientation == Orientation.positive:
+                retdict['orientation'] = '+'    
+            elif self.orientation == Orientation.negative:
+                retdict['orientation'] = '-'        
+            else:
+                retdict['orientation'] = enum2str(self.orientation)    
+            
+            return retdict
+
+class Object(_SignalObjectCommon):
+    """ creates an Object
+
+        Parameters
+        ----------
+            _SignalObjectCommon: super-class with common attributes of Signal / Object
+
+        Attributes
+        ----------
+            s (float): s-coordinate of Object (init in super-class)
+
+            t (float): t-coordinate of Object (init in super-class)
+
+            height (float): height of Object (init in super-class)
+
+            object_id (string): id of Object (init in super-class)
+
+            type (ObjectType or string): type of Object (typically enum ObjectType) (init in super-class)
+
+            subtype (string): subtype for further specification of Object (init in super-class)
+
+            dynamic (Dynamic): specifies if Object is static or dynamic (init in super-class)
+            
+            name (string): name for identification of Object (init in super-class)
+            
+            zOffset (float): vertical offset of Object with respect to centerline (init in super-class)
+            
+            orientation (Orientation): orientation of Object with respect to road (init in super-class)
+            
+            pitch (float): pitch angle (rad) of Object relative to the inertial system (xy-plane) (init in super-class)
+            
+            roll (float): roll angle (rad) of Object after applying pitch, relative to the inertial system (x’’y’’-plane) (init in super-class)
+            
+            width (float): width of the Object (init in super-class)
+        
+            validLength (float): validLength
+            
+            length (float): width of the Object (shall not be used with radius)
+            
+            hdg (float): heading angle (rad) of the Object relative to road direction
+            
+            radius (float): radius of the Object (shall not be used with width/length)
+            
+            _repeats ([dict]): list of dictionary containing attributes for optional subelement for repeating Objects to be filled by repeat method
+            
+            _usedIDs ([str]): list of used IDs shared among all instances of Object to throw warning when ID is not unique 
+
+        Methods
+        -------
+            repeat()
+                adds a dictionary to _repeats[] list to create a subelement for repeating the Object
+                
+            get_element()
+                Returns the full ElementTree of the class
+
+            get_attributes()
+                Returns a dictionary of all attributes of FileHeader
+
+    """
+    
+    _usedIDs = []
+    
+    def __init__(self,s,t,height,object_id,dynamic=Dynamic.no,name='defaultName',zOffset=0,Type=ObjectType.none,subtype=None,validLength=0,orientation=Orientation.none,hdg=0,pitch=0,roll=0,width=None,length=None,radius=None):
+        """ initalizes the Object
+
+        Parameters
+        ----------
+            s (float): s-coordinate of Object (init in super-class)
+
+            t (float): t-coordinate of Object (init in super-class)
+
+            height (float): height of Object (init in super-class)
+
+            object_id (string): id of Object (init in super-class)
+
+            type (ObjectType or string): type of Object (typically enum ObjectType) (init in super-class)
+                Default: ObjectType.none
+            subtype (string): subtype for further specification of Object (init in super-class)
+                Default: None
+            dynamic (Dynamic): specifies if Object is static or dynamic (init in super-class)
+                Default: Dynamic.no            
+            name (string): name for identification of Object (init in super-class)
+                Default: 'defaultName'                   
+            zOffset (float): vertical offset of Object with respect to centerline (init in super-class)
+                Default: 0                        
+            orientation (Orientation): orientation of Object with respect to road (init in super-class)
+                Default: Orientation.none                                    
+            pitch (float): pitch angle (rad) of Object relative to the inertial system (xy-plane) (init in super-class)
+                Default: 0                                 
+            roll (float): roll angle (rad) of Object after applying pitch, relative to the inertial system (x’’y’’-plane) (init in super-class)
+                Default: 0                                             
+            width (float): width of the Object (init in super-class)
+                Default: None                                         
+            validLength (float): validLength
+                Default: 0                                             
+            length (float): width of the Object (shall not be used with radius)
+                Default: None                                            
+            hdg (float): heading angle (rad) of the Object relative to road direction
+                Default: 0
+            radius (float): radius of the Object (shall not be used with width/length)
+                Default: None
+
+        """
+        #get attributes that are common with signals
+        super().__init__(s,t,height,object_id,Type,subtype,dynamic,name,zOffset,orientation,pitch,roll,width)
+                
+        #attributes that differ from signals
+        self.validLength = validLength
         self.length = length
+        self.hdg = hdg
         self.radius = radius
         
+        #list for repeat entries
         self._repeats = []
         
-        
+        if self.object_id in self._usedIDs:
+            print ("Warning: id",self.object_id,"has already been used for another object and is not unique.")
+        self._usedIDs.append(object_id)
+    
         #check if width/length combination or radius was provided and ensure working defaults 
         if radius is not None and (width is not None or length is not None):
             print ("Object with id",self.object_id,"was provided with radius, width and/or length. Provide either radius or width and length. Using radius as fallback.")
@@ -117,42 +263,44 @@ class Object():
         self._repeats[-1]['length']=str(repeatLength)
         self._repeats[-1]['distance']=str(repeatDistance)
         
+        def infoFallback(object_id, attributeName):
+            print ("Info: Using data of parent object with id",object_id,"as attribute",attributeName,"was not specified for repeat entry.")
+                  
         #ensuring that all attributes that are required according to OpenDRIVE 1.6 are filled - for convenience the ones of the parent object are used
-        #if not provided specifically  
-        
+        #if not provided specifically          
         if sStart == None:
             self._repeats[-1]['s']=str(self.s)
-            print ("Info: no s-coordinate specified for repeating object with id",self.object_id ,", using s-coordinate of original object instead.")
+            infoFallback(self.object_id, 's')
         else:
             self._repeats[-1]['s']=str(sStart)           
         if tStart == None:
             self._repeats[-1]['tStart']=str(self.t)
-            print ("Info: no starting t-coordinate specified for repeating object with id",self.object_id ,", using t-coordinate of original object instead.")
+            infoFallback(self.object_id, 'tStart')
         else:
             self._repeats[-1]['tStart']=str(tStart)
         if tEnd == None:
             self._repeats[-1]['tEnd']=str(self.t)
-            print ("Info: no ending t-coordinate specified for repeating object with id",self.object_id ,", using t-coordinate of original object instead.")
+            infoFallback(self.object_id, 'tEnd')
         else:
             self._repeats[-1]['tEnd']=str(tEnd)
         if heightStart == None:
             self._repeats[-1]['heightStart']=str(self.height)
-            print ("Info: no starting height specified for repeating object with id",self.object_id ,", using height of original object instead.")
+            infoFallback(self.object_id, 'heightStart')
         else:
             self._repeats[-1]['heightStart']=str(heightStart)
         if heightEnd == None:
             self._repeats[-1]['heightEnd']=str(self.height)
-            print ("Info: no ending height specified for repeating object with id",self.object_id ,", using height of original object instead.")
+            infoFallback(self.object_id, 'heightEnd')
         else:
             self._repeats[-1]['heightEnd']=str(heightEnd)
         if zOffsetStart == None:
             self._repeats[-1]['zOffsetStart']=str(self.zOffset)
-            print ("Info: no starting zOffset specified for repeating object with id",self.object_id ,", using zOffset of original object instead.")
+            infoFallback(self.object_id, 'zOffsetStart')
         else:
             self._repeats[-1]['zOffsetStart']=str(zOffsetStart)
         if zOffsetEnd == None:
             self._repeats[-1]['zOffsetEnd']=str(self.zOffset)
-            print ("Info: no ending zOffset specified for repeating object with id",self.object_id ,", using zOffset of original object instead.")
+            infoFallback(self.object_id, 'zOffsetEnd')
         else:
             self._repeats[-1]['zOffsetEnd']=str(zOffsetEnd)
         
@@ -175,32 +323,17 @@ class Object():
         """ returns the attributes of the Object as a dict
 
         """
-        retdict = {}
+        retdict = super().get_common_attributes()
         retdict['type'] = enum2str(self.Type)
-        retdict['id'] = str(self.object_id)
-        retdict['s'] = str(self.s)
-        retdict['t'] = str(self.t)
-        retdict['height'] = str(self.height)
-        retdict['subtype'] = str(self.subtype)
-        retdict['dynamic'] = enum2str(self.dynamic)
-        retdict['name'] = str(self.name)
-        retdict['zOffset'] = str(self.zOffset)
         retdict['validLength'] = str(self.validLength)
         retdict['hdg'] = str(self.hdg)        
-        retdict['pitch'] = str(self.pitch)
-        retdict['roll'] = str(self.roll)
-        
-        if self.orientation == Orientation.positive:
-            retdict['orientation'] = '+'    
-        elif self.orientation == Orientation.negative:
-            retdict['orientation'] = '-'        
-        else:
-            retdict['orientation'] = enum2str(self.orientation)    
+            
         
         if self.radius is not None:
-            retdict['radius'] = str(self.radius)                    
+            retdict['radius'] = str(self.radius)
+            #remove width entry that comes per default from super-class      
+            retdict.pop('width')               
         else:
-            retdict['width'] = str(self.width)
             retdict['length'] = str(self.length)
         
         return retdict
@@ -215,6 +348,4 @@ class Object():
 
         return element
     
-#TODO Adjust all comments
-#TODO Allow repeated
-#TODO Generate tests
+#TODO Adapt examples
