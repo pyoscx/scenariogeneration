3641293dbca7cd80ec9891d0ac46a6da0634c2a7
Kaths, Jakob <Jakob.Kaths@vector.com>
2021-01-28 16:18:27 +0100

    Use common base class for signals and objects

--- highway_example.py
+++ highway_example.py
@@ -1,4 +1,12 @@
-import pyodrx 
+
+import pyodrx.geometry
+import pyodrx.helpers 
+import pyodrx.opendrive 
+import pyodrx.lane 
+import pyodrx.enumerations
+import pyodrx.links 
+import pyodrx.generators
+import pyodrx.objects
 import os
 
 # create some simple roads
@@ -6,7 +14,7 @@ roads= []
 # start road
 roads.append(pyodrx.create_road([pyodrx.Spiral(-0.004,0.00001,100), pyodrx.Spiral(0.00001,0.005,50), pyodrx.Arc(0.005,50)],id =0,left_lanes=3,right_lanes=4))
 # intermittent road
-roads.append(pyodrx.create_road([pyodrx.Spiral(0.0001,0.003,65), pyodrx.Spiral(0.003,0.00001,50) ],id =1,left_lanes=3,right_lanes=3))
+roads.append(pyodrx.create_road([pyodrx.Line(30), ],id =1,left_lanes=3,right_lanes=3))
 
 
 #exit road
@@ -25,6 +33,8 @@ roads.append(pyodrx.create_road([pyodrx.Spiral(0.004,0.000001,50) ],id =7,left_l
 # entry road
 roads.append(pyodrx.create_road(pyodrx.Arc(0.004,60),id =8,left_lanes=1,right_lanes=0))
 
+
+# roads.append(pyodrx.create_road([pyodrx.Line(30), ],id =9,left_lanes=3,right_lanes=3))
 
 # add predecessors and succesors to the non junction roads
 roads[0].add_successor(pyodrx.ElementType.junction,1)
@@ -54,6 +64,52 @@ roads[7].add_successor(pyodrx.ElementType.road,8,pyodrx.ContactPoint.start)
 # add connection to the entry road
 roads[8].add_predecessor(pyodrx.ElementType.junction,2)
 
+# roads[9].add_predecessor(pyodrx.ElementType.road,1,pyodrx.ContactPoint.end)
+# roads[9].add_successor(pyodrx.ElementType.junction,2)
+brokenRM = pyodrx.RoadMark(pyodrx.RoadMarkType.broken,0.2,rule=pyodrx.MarkRule.no_passing)
+
+
+
+guardRail0Right = pyodrx.Object(5,-13,0.3,zOffset=0.4,Type=pyodrx.ObjectType.barrier,object_id="100",name="guardRail")
+guardRail0Right.repeat(2, 0, sStart=0,zOffsetStart=0,zOffsetEnd=0.4)
+guardRail0Right.repeat(198, 0, sStart=2)
+
+guardRail0Left = pyodrx.Object(5,9.5,0.3,zOffset=0.4,Type=pyodrx.ObjectType.barrier,object_id="100",name="guardRail",hdg=3.14)
+guardRail0Left.repeat(2, 0, sStart=0,zOffsetStart=0,zOffsetEnd=0.4)
+guardRail0Left.repeat(198, 0, sStart=2)
+
+pole0Right = pyodrx.Object(5,-13.2,1,zOffset=-0.05,Type=pyodrx.ObjectType.pole,object_id="1",name="permanentDelineator")
+pole0Left = pyodrx.Object(5,9.7,1,zOffset=-0.05,Type=pyodrx.ObjectType.pole,object_id="2",name="permanentDelineator",hdg=3.14)
+pole0Right.repeat(200, 50)
+pole0Left.repeat(200, 50)
+
+
+jerseyBarrier0 = pyodrx.Object(0,0,0.75,zOffset=0,Type=pyodrx.ObjectType.barrier,object_id="100",name="jerseyBarrier")
+jerseyBarrier0.repeat(200, 0)
+
+jerseyBarrier1 = pyodrx.Object(0,0,0.75,zOffset=0,Type=pyodrx.ObjectType.barrier,object_id="101",name="jerseyBarrier")
+jerseyBarrier1.repeat(30, 0)
+
+jerseyBarrier3 = pyodrx.Object(0,0,0.75,zOffset=0,Type=pyodrx.ObjectType.barrier,object_id="103",name="jerseyBarrier")
+jerseyBarrier3.repeat(50, 0)
+
+jerseyBarrier5 = pyodrx.Object(0,0,0.75,zOffset=0,Type=pyodrx.ObjectType.barrier,object_id="105",name="jerseyBarrier")
+jerseyBarrier5.repeat(105, 0)
+
+jerseyBarrier6 = pyodrx.Object(0,0,0.75,zOffset=0,Type=pyodrx.ObjectType.barrier,object_id="106",name="jerseyBarrier")
+jerseyBarrier6.repeat(30, 0)
+
+roads[0].lanes.lanesections[0].rightlanes[0].add_roadmark(brokenRM)
+
+roads[0].add_object(pole0Right)
+roads[0].add_object(pole0Left)
+roads[0].add_object(jerseyBarrier0)
+roads[0].add_object(guardRail0Right)
+roads[0].add_object(guardRail0Left)
+roads[1].add_object(jerseyBarrier1)
+roads[3].add_object(jerseyBarrier3)
+roads[5].add_object(jerseyBarrier5)
+roads[6].add_object(jerseyBarrier6)
 # create the junction struct 
 exit_junction = pyodrx.create_junction(roads[3:5],1,roads[0:3])
 entry_junction = pyodrx.create_junction(roads[6:8],2,[roads[x] for x in [1,5,8]])
@@ -66,4 +122,11 @@ odr.add_junction(exit_junction)
 odr.add_junction(entry_junction)
 
 # display the road using esmini
-pyodrx.run_road(odr,os.path.join('..','..','esmini'))
+odr.write_xml(os.path.basename(__file__).replace('.py','.xodr'))
+
+##13. Print the .xodr file
+#pyodrx.prettyprint(odr.get_element())
+
+##13. Run the .xodr with esmini 
+#pyodrx.run_road(odr,os.path.join('..','pyoscx','esmini'))
+
