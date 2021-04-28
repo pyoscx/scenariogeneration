a782d20cae3356f494fd20540b27900a5bc7c303
mander76 <mikael.andersson.5@volvocars.com>
2021-04-28 15:46:10 +0200

    Added a CatalogReader that will read a Catalog and return the give scenariogeneration.xosc object

--- scenariogeneration/xosc/exceptions.py
+++ scenariogeneration/xosc/exceptions.py
@@ -7,3 +7,8 @@ class NoActionsDefinedError(Exception):
     """ Raised when no actions are defined in a class
     """
     pass
+
+class NoCatalogFoundError(Exception):
+    """ Raised when no catalog is found with the given name
+    """
+    pass
