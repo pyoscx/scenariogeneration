043c96df45ef72a4179f3741b17a934ef640e45b
mander76 <mikael.andersson.5@volvocars.com>
2021-06-10 10:35:34 +0200

    fixed title in documentation generation

--- docu/generate_documentation.sh
+++ docu/generate_documentation.sh
@@ -10,4 +10,6 @@ cd generated
 mv for_documentation.html index.html
 
 sed -i "s+Module <code>for_documentation</code>+Welcome to scenariogeneration+" index.html
+sed -i "s+for_documentation API documentation+Welcome to scenariogeneration+" index.html
+
 cd ../docu
