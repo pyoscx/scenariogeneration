b4b6d3851acb3f03aea13d43de9237788000e853
mander76 <mikael.andersson.5@volvocars.com>
2022-01-28 07:50:46 +0100

    removed esmini runner from example

--- examples/xodr/highway_exit_with_lane_types.py
+++ examples/xodr/highway_exit_with_lane_types.py
@@ -53,5 +53,5 @@ odr.add_junction(direct_junction)
 odr.write_xml(os.path.basename(__file__).replace('.py','.xodr'))
 
 # uncomment the following lines to display the road using esmini
-from scenariogeneration import esmini
-esmini(odr,os.path.join('esmini'))
+# from scenariogeneration import esmini
+# esmini(odr,os.path.join('esmini'))
