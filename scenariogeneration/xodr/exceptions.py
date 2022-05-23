fbed94b2361dde33d7163aae587178c97f0e4895
mander76 <mikael.andersson.5@volvocars.com>
2022-05-23 10:10:12 +0200

    adding method for adding junctioncreators, and check that no roads are added with same id

--- scenariogeneration/xodr/exceptions.py
+++ scenariogeneration/xodr/exceptions.py
@@ -50,3 +50,7 @@ class GeneralIssueInputArguments(Exception):
     """Raise when there is something wrong in the input arguments"""
 
     pass
+
+class IdAlreadyExists(Exception):
+    """ Raise when there is a clash between ids"""
+    pass
