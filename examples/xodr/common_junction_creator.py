fdc02801f011f05ee335506d634b944b274a9fa0
mander76 <mikael.andersson.5@volvocars.com>
2022-05-24 08:08:33 +0200

    Fixing exampels for junction creator

---
+++ examples/xodr/common_junction_creator.py
@@ -0,0 +1,72 @@
+"""
+    Example of how to create a highway entry using CommonJunctionCreator
+
+    Some features used:
+
+    - create_road
+
+    - add_successor/add_predecessor with and without the lane_offset option, and the direct_junction input
+
+    - CommonJunctionCreator
+"""
+
+import os
+from scenariogeneration import xodr
+
+# initalize the junction creator
+junction_id = 100
+junction_creator = xodr.DirectJunctionCreator(id = junction_id, name='my direct junction')
+
+# create 3 roads, and add the successor/predecessor junction
+
+road1 = xodr.create_road(xodr.Line(100), id=1, left_lanes=2, right_lanes=2)
+road2 = xodr.create_road(xodr.Line(100), id=2, left_lanes=1, right_lanes=1)
+road3 = xodr.create_road(xodr.Line(100), id=3, left_lanes=2, right_lanes=2)
+
+# create direct junction connection to all common lanes between the main roads
+
+junction_creator = xodr.CommonJunctionCreator(id = 100, name='my_junction')
+
+junction_creator.add_incoming_road_cartesian_geometry(road1, 
+            x = 0, 
+            y = 0, 
+            heading=0, 
+            road_connection='successor')
+
+junction_creator.add_incoming_road_cartesian_geometry(road2, 
+            x = 50, 
+            y = 50, 
+            heading=3.1415*3/2, 
+            road_connection='predecessor')
+
+junction_creator.add_incoming_road_cartesian_geometry(road3, 
+            x = 100, 
+            y = 0, 
+            heading=-3.1415, 
+            road_connection='predecessor')
+
+
+# create the opendrive
+odr = xodr.OpenDrive('my_road')
+
+# add the roads
+junction_creator.add_connection(road_one_id = 1, road_two_id =3)
+junction_creator.add_connection(road_one_id = 1, road_two_id = 2, lane_one_id= 2,lane_two_id=1)
+junction_creator.add_connection(road_one_id = 2, road_two_id = 3, lane_one_id= -1, lane_two_id=2)
+
+odr.add_road(road1)
+odr.add_road(road2)
+odr.add_road(road3)
+
+# add the junction creator
+odr.add_junction_creator(junction_creator)
+
+# adjust the roads and lanes
+odr.adjust_roads_and_lanes()
+
+# write the OpenDRIVE file as xodr using current script name
+odr.write_xml(os.path.basename(__file__).replace(".py", ".xodr"))
+
+# uncomment the following lines to display the road using esmini
+from scenariogeneration import esmini
+esmini(odr,os.path.join('../../esmini'))
---
+++ examples/xodr/direct_junction_creator.py
@@ -0,0 +1,61 @@
+"""
+    Example of how to create a highway exits using DirectJunctionCreator
+
+    Some features used:
+
+    - create_road
+
+    - add_successor/add_predecessor with and without the lane_offset option, and the direct_junction input
+
+    - DirectJunctionCreator
+"""
+
+import os
+from scenariogeneration import xodr
+
+# initalize the junction creator
+junction_id = 100
+junction_creator = xodr.DirectJunctionCreator(id = junction_id, name='my direct junction')
+
+# create 4 roads, and add the successor/predecessor junction
+start_road = xodr.create_road([xodr.Line(200)], id= 1, left_lanes=2, right_lanes=3)
+start_road.add_successor(xodr.ElementType.junction, junction_id)
+
+continuation_road = xodr.create_road([xodr.Line(200)], id= 2, left_lanes=3, right_lanes=2)
+continuation_road.add_predecessor(xodr.ElementType.junction, junction_id)
+
+exit_road_right = xodr.create_road(xodr.Spiral(-0.00001, -0.001,100), id = 10,left_lanes=0, right_lanes=1)
+exit_road_right.add_predecessor(xodr.ElementType.junction, junction_id)
+
+exit_road_left = xodr.create_road(xodr.Spiral(0.01,0.0001,150), id = 20, left_lanes=1, right_lanes=0)
+exit_road_left.add_successor(xodr.ElementType.junction, junction_id)
+
+# create direct junction connection to all common lanes between the main roads
+junction_creator.add_connection(incoming_road = start_road, linked_road = continuation_road)
+
+# create the connection to the exit roads with only the specific lanes
+junction_creator.add_connection(incoming_road = start_road, linked_road = exit_road_right, incoming_lane_ids= -3, linked_lane_ids=-1)
+junction_creator.add_connection(incoming_road = exit_road_left, linked_road = continuation_road, incoming_lane_ids=1, linked_lane_ids=3)
+
+
+# create the opendrive
+odr = xodr.OpenDrive('my_road')
+
+# add the roads
+odr.add_road(start_road)
+odr.add_road(continuation_road)
+odr.add_road(exit_road_right)
+odr.add_road(exit_road_left)
+
+# add the junction creator
+odr.add_junction_creator(junction_creator)
+
+# adjust the roads and lanes
+odr.adjust_roads_and_lanes()
+
+# write the OpenDRIVE file as xodr using current script name
+odr.write_xml(os.path.basename(__file__).replace(".py", ".xodr"))
+
+# uncomment the following lines to display the road using esmini
+# from scenariogeneration import esmini
+# esmini(odr,os.path.join('esmini'))
