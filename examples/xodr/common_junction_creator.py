7aa197aa1a77899def136fc286d557876e24b62d
mander76 <mikael.andersson.5@volvocars.com>
2022-05-30 14:43:03 +0200

    removed old direct junction exampels

--- examples/xodr/common_junction_creator.py
+++ examples/xodr/common_junction_creator.py
@@ -68,5 +68,5 @@ odr.adjust_roads_and_lanes()
 odr.write_xml(os.path.basename(__file__).replace(".py", ".xodr"))
 
 # uncomment the following lines to display the road using esmini
-from scenariogeneration import esmini
-esmini(odr,os.path.join('../../esmini'))
+# from scenariogeneration import esmini
+# esmini(odr,os.path.join('esmini'))
