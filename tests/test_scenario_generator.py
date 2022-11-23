36fc4bb60282271a1ffe3e0445efed78332dc197
mander76 <mikael.andersson.5@volvocars.com>
2022-11-23 11:53:59 +0100

    fixing test for new writer interface

--- tests/test_scenario_generator.py
+++ tests/test_scenario_generator.py
@@ -1,11 +1,11 @@
 """
   scenariogeneration
   https://github.com/pyoscx/scenariogeneration
- 
+
   This Source Code Form is subject to the terms of the Mozilla Public
   License, v. 2.0. If a copy of the MPL was not distributed with this
   file, You can obtain one at https://mozilla.org/MPL/2.0/.
- 
+
   Copyright (c) 2022 The scenariogeneration Authors.
 
 """
@@ -18,7 +18,7 @@ from scenariogeneration import ScenarioGenerator
 
 
 class writer_dummy:
-    def write_xml(self, filename):
+    def write_xml(self, filename, prettyprint):
         with open(filename, "w") as f:
             pass
 
