6ce9f426c6d14d6d8591e91e6a8bccb000a0be26
mander76 <mikael.andersson.5@volvocars.com>
2023-03-02 13:47:27 +0100

    adding tmpdir to filewriting in tests

--- tests/test_xml_validation.py
+++ tests/test_xml_validation.py
@@ -24,7 +24,7 @@ import pytest
         "withcontroller",
     ],
 )
-def test_OSC_1_0(python_file):
+def test_OSC_1_0(tmpdir, python_file):
     sys.path.insert(
         1, os.path.join(os.path.split(__file__)[0], os.pardir, "examples", "xosc")
     )
@@ -38,7 +38,7 @@ def test_OSC_1_0(python_file):
     imp = importlib.import_module(python_file)
     sce = imp.Scenario()
     sce.open_scenario_version = 0
-    xosc, _ = sce.generate(".")
+    xosc, _ = sce.generate(tmpdir)
     assert validator.validate(etree.parse(xosc[0]))
 
 
@@ -61,7 +61,7 @@ def test_OSC_1_0(python_file):
         "withcontroller",
     ],
 )
-def test_OSC_1_1(python_file):
+def test_OSC_1_1(tmpdir, python_file):
     sys.path.insert(
         1, os.path.join(os.path.split(__file__)[0], os.pardir, "examples", "xosc")
     )
@@ -75,7 +75,7 @@ def test_OSC_1_1(python_file):
     imp = importlib.import_module(python_file)
     sce = imp.Scenario()
     sce.open_scenario_version = 1
-    xosc, _ = sce.generate(".")
+    xosc, _ = sce.generate(tmpdir)
     assert validator.validate(etree.parse(xosc[0]))
 
 
@@ -99,7 +99,7 @@ def test_OSC_1_1(python_file):
         "withcontroller",
     ],
 )
-def test_OSC_1_2(python_file):
+def test_OSC_1_2(tmpdir, python_file):
     sys.path.insert(
         1, os.path.join(os.path.split(__file__)[0], os.pardir, "examples", "xosc")
     )
@@ -113,5 +113,5 @@ def test_OSC_1_2(python_file):
     imp = importlib.import_module(python_file)
     sce = imp.Scenario()
     sce.open_scenario_version = 2
-    xosc, _ = sce.generate(".")
+    xosc, _ = sce.generate(tmpdir)
     assert validator.validate(etree.parse(xosc[0]))
