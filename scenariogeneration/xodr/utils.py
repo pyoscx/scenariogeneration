69d2761c9295e6b295943e24d8c37bbb9ac0d700
atingber <andreas.tingberg@volvocars.com>
2025-06-04 16:16:14 +0200

    typehinting and docstring formatting for utils.py

--- scenariogeneration/xodr/utils.py
+++ scenariogeneration/xodr/utils.py
@@ -11,28 +11,32 @@ Copyright (c) 2022 The scenariogeneration Authors.
 """
 
 import xml.etree.ElementTree as ET
-
+from typing import Optional
 import numpy as np
 
 from ..helpers import enum2str
 from .enumerations import ContactPoint
 
 
-def get_lane_sec_and_s_for_lane_calc(road, contact_point):
-    """Helping function to get a lane_section and s coordinate for
-    calculations, at either the end or start of a road.
+def get_lane_sec_and_s_for_lane_calc(
+    road: "Road", contact_point: "ContactPoint"
+) -> tuple[int, float]:
+    """Get a lane section and s-coordinate for calculations at either the
+    start or end of a road.
 
     Parameters
     ----------
-        road (Road): the road to be used
-
-        contact_point (ContactPoint): start or end of the road
+    road : Road
+        The road to be used.
+    contact_point : ContactPoint
+        Start or end of the road.
 
     Returns
     -------
-        lanesection (int): 0 or -1
-
-        s (float): the s coordinate at start or end of the road
+    tuple[int, float]
+        A tuple containing:
+        - lanesection (int): 0 or -1, indicating the relevant lane section.
+        - s (float): The s-coordinate at the start or end of the road.
     """
 
     relevant_s = 0
@@ -49,27 +53,35 @@ def get_lane_sec_and_s_for_lane_calc(road, contact_point):
     return relevant_lanesection, relevant_s
 
 
-def get_coeffs_for_poly3(length, lane_offset, zero_start, lane_width_end=None):
-    """get_coeffs_for_poly3 creates the coefficients for a third degree
-    polynomial, can be used for all kinds of descriptions in xodr.
+def get_coeffs_for_poly3(
+    length: float,
+    lane_offset: float,
+    zero_start: bool,
+    lane_width_end: Optional[float] = None,
+) -> tuple[float, float, float, float]:
+    """Create the coefficients for a third-degree polynomial, which can be
+    used for various descriptions in OpenDRIVE.
 
-    Assuming that the derivative is 0 at the start and end of the segment.
+    Assumes that the derivative is 0 at the start and end of the segment.
 
     Parameters
     ----------
-        length (float): length of the segment in the s direction
+    length : float
+        Length of the segment in the s-direction.
+    lane_offset : float
+        The lane offset (width) of the lane.
+    zero_start : bool
+        If True, starts with zero and ends with `lane_offset` width.
+        If False, starts with `lane_offset` and ends with zero width.
+    lane_width_end : float, optional
+        Specifies the ending lane width for lanes that may start and end
+        with different widths. Default is None.
 
-        lane_offset (float): the lane offset (width) of the lane
-
-        zero_start (bool): True; start with zero and ends with lane_offset width,
-                           False; start with lane_offset and ends with zero width
-
-        lane_width_end (float): specify the ending lane width for lanes that may start
-                                and end with different widths
-
-    Return
-    ------
-        coefficients (float,float,float,float): polynomial coefficients corresponding to "a, b, c, d" in the OpenDrive polynomials
+    Returns
+    -------
+    tuple[float, float, float, float]
+        Polynomial coefficients corresponding to "a, b, c, d" in the
+        OpenDRIVE polynomials.
     """
     # might be expanded for other cases, not now if needed yet though
     start_heading = 0
@@ -99,36 +111,40 @@ def get_coeffs_for_poly3(length, lane_offset, zero_start, lane_width_end=None):
 
 
 class XodrBase:
-    """Sets up common functionality for xodr generating classes by enabling
+    """Sets up common functionality for xodr-generating classes by enabling
     userdata inputs.
-
-    Parameters
-    ----------
 
     Attributes
     ----------
-        user_data (list of UserData): all userdata added
-
-        includes (list of str): all includes (filenames)
-
-        data_quality (DataQuality): All
+    user_data : list[UserData]
+        All userdata added to the xodr entry.
+    data_quality : DataQuality, optional
+        Data quality information for the xodr entry.
 
     Methods
     -------
-
-        add_userdata(userdata)
-            adds a userdata to the xodr entry
-
-        add_dataquality(dataquality)
-            adds dataquality to the xodr entry
+    add_userdata(userdata)
+        Adds a userdata entry to the xodr entry.
+    add_dataquality(dataquality)
+        Adds a data quality entry to the xodr entry.
+    _add_additional_data_to_element(element)
+        Adds userdata and data quality to an XML element.
     """
 
-    def __init__(self):
-        """Initalize the UserData."""
+    def __init__(self) -> None:
+        """Initialize the XodrBase instance.
+
+        Attributes
+        ----------
+        user_data : list[UserData]
+            A list to store all userdata entries added to the xodr entry.
+        data_quality : Optional[DataQuality]
+            Data quality information for the xodr entry.
+        """
         self.user_data = []
         self.data_quality = None
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         if (
             self.user_data == other.user_data
             and self.data_quality == other.data_quality
@@ -137,30 +153,55 @@ class XodrBase:
 
         return False
 
-    def add_userdata(self, userdata):
-        """Adds a userdata entry to the xodr entry.
+    def add_userdata(self, userdata: "UserData") -> None:
+        """Add a userdata entry to the xodr entry.
 
         Parameters
         ----------
-            userdata (Userdata): the data to be added
+        userdata : UserData
+            The userdata to be added.
+
+        Raises
+        ------
+        TypeError
+            If the provided userdata is not of type UserData.
         """
         if not isinstance(userdata, UserData):
             raise TypeError("userdata is not of type UserData.")
         self.user_data.append(userdata)
 
-    def add_dataquality(self, dataquality):
-        """Adds a dataquality entry to the xodr entry.
+    def add_dataquality(self, dataquality: "DataQuality") -> None:
+        """Add a data quality entry to the xodr entry.
 
         Parameters
         ----------
-            dataquality (DataQuality): the data to be added
+        dataquality : DataQuality
+            The data quality to be added.
+
+        Raises
+        ------
+        TypeError
+            If the provided dataquality is not of type DataQuality.
         """
         if not isinstance(dataquality, DataQuality):
             raise TypeError("dataquality is not of type DataQuality.")
         self.data_quality = dataquality
 
-    def _add_additional_data_to_element(self, element):
-        """Returns the elementTree of the Junction."""
+    def _add_additional_data_to_element(
+        self, element: ET.Element
+    ) -> ET.Element:
+        """Add userdata and data quality to an XML element.
+
+        Parameters
+        ----------
+        element : ET.Element
+            The XML element to which userdata and data quality will be added.
+
+        Returns
+        -------
+        ET.Element
+            The updated XML element.
+        """
         for ud in self.user_data:
             element.append(ud.get_element())
         if self.data_quality:
@@ -169,51 +210,66 @@ class XodrBase:
 
 
 class UserData:
-    """Sets up addtional data for any entry of OpenDRIVE.
-
-    Parameters
-    ----------
-        code (str): code of the userdata
-
-        value (str): value of the userdata
-            Default: None
+    """Sets up additional data for any entry of OpenDRIVE.
 
     Attributes
     ----------
-        code (str): code of the userdata
-
-        value (str): value of the userdata
-
-        user_data_content (list of ET.element): list of all extra elements added
+    code : str
+        Code of the userdata.
+    value : str, optional
+        Value of the userdata.
+    user_data_content : list[ET.Element]
+        List of all extra elements added.
 
     Methods
     -------
-        get_element()
-            Returns the full ElementTree of the class
-
-        get_attributes()
-            Returns a dictionary of all attributes of the class
-
-        add_userdata_content(ET.element)
-            adds extra elements to the userdata
+    add_userdata_content(content)
+        Adds extra elements to the userdata.
+    get_element()
+        Returns the full ElementTree representation of the UserData.
+    get_attributes()
+        Returns a dictionary of all attributes of the UserData.
     """
 
-    def __init__(self, code, value=None):
-        """Initalize the UserData."""
+    def __init__(self, code: str, value: Optional[str] = None) -> None:
+        """Initialize the UserData.
+
+        Parameters
+        ----------
+        code : str
+            Code of the userdata.
+        value : str, optional
+            Value of the userdata. Default is None.
+        """
         self.code = code
         self.value = value
         self.userdata_content = []
 
-    def add_userdata_content(self, content):
-        """add_userdata_content adds extra elements to userdata.
+    def add_userdata_content(self, content: ET.Element) -> None:
+        """Add extra elements to the userdata.
 
         Parameters
         ----------
-            content (ET.element): the element to be added
+        content : ET.Element
+            The element to be added.
         """
         self.userdata_content.append(content)
 
-    def _element_equals(self, e1, e2):
+    def _element_equals(self, e1: ET.Element, e2: ET.Element) -> bool:
+        """Compare two XML elements for equality.
+
+        Parameters
+        ----------
+        e1 : ET.Element
+            The first element to compare.
+        e2 : ET.Element
+            The second element to compare.
+
+        Returns
+        -------
+        bool
+            True if the elements are equal, False otherwise.
+        """
         if e1.tag != e2.tag:
             return False
         if e1.text != e2.text:
@@ -226,7 +282,7 @@ class UserData:
             return False
         return all(self._elements_equal(c1, c2) for c1, c2 in zip(e1, e2))
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         if isinstance(other, UserData):
             if self.get_attributes() == other.get_attributes() and len(
                 self.userdata_content
@@ -239,16 +295,28 @@ class UserData:
                 return True
         return False
 
-    def get_attributes(self):
-        """Returns the attributes as a dict of the JunctionGroup."""
+    def get_attributes(self) -> dict[str, str]:
+        """Return the attributes of the UserData as a dictionary.
+
+        Returns
+        -------
+        dict[str, str]
+            A dictionary containing the attributes of the UserData.
+        """
         retdict = {}
         retdict["code"] = str(self.code)
         if self.value is not None:
             retdict["value"] = str(self.value)
         return retdict
 
-    def get_element(self):
-        """Returns the elementTree of the Junction."""
+    def get_element(self) -> ET.Element:
+        """Return the ElementTree representation of the UserData.
+
+        Returns
+        -------
+        ET.Element
+            The XML ElementTree representation of the UserData.
+        """
         element = ET.Element("userData", attrib=self.get_attributes())
         for i in self.userdata_content:
             element.append(i)
@@ -260,76 +328,97 @@ class DataQuality:
 
     Attributes
     ----------
-        date (str): code of the userdata
-
-        post_processing (RawDataPostProcessing): postprocessing definition
-
-        source (RawDataSource): source of the data
-
-        post_processing_comment (str): comment of the postprocessing
-
-        source_comment (str): comment of the soure
-
-        xy_abs (float): absolute xy error
-
-        xy_rel (float): relative xy error
-
-        z_abs (float): absolute z error
-
-        z_rel (float): relative z error
+    date : str, optional
+        Date of the data.
+    post_processing : RawDataPostProcessing, optional
+        Postprocessing definition.
+    source : RawDataSource, optional
+        Source of the data.
+    post_processing_comment : str, optional
+        Comment about the postprocessing.
+    source_comment : str, optional
+        Comment about the source.
+    xy_abs : float, optional
+        Absolute xy error.
+    xy_rel : float, optional
+        Relative xy error.
+    z_abs : float, optional
+        Absolute z error.
+    z_rel : float, optional
+        Relative z error.
 
     Methods
     -------
-        get_element()
-            Returns the full ElementTree of the class
-
-        get_attributes()
-            Returns a dictionary of all attributes of the class
-
-        add_raw_data_info(date, post_processing, source, post_processing_comment, source_comment)
-            adds raw data info
-
-        add_error(xy_abs, xy_rel, z_abs,z_rel)
-            adds error info
+    add_raw_data_info(date, post_processing, source, post_processing_comment, source_comment)
+        Adds raw data information.
+    add_error(xy_abs, xy_rel, z_abs, z_rel)
+        Adds error information.
+    get_element()
+        Returns the full ElementTree representation of the DataQuality.
     """
 
-    def __init__(self):
-        """Initalize the UserData."""
-        self.date = None
-        self.post_processing = None
-        self.source = None
-        self.post_processing_comment = None
-        self.source_comment = None
-        self.xy_abs = None
-        self.xy_rel = None
-        self.z_abs = None
-        self.z_rel = None
-        self._error_added = False
-        self._raw_data_added = False
+    def __init__(self) -> None:
+        """Initialize the DataQuality instance.
+
+        Attributes
+        ----------
+        date : Optional[str]
+            Date of the data.
+        post_processing : Optional[RawDataPostProcessing]
+            Postprocessing definition.
+        source : Optional[RawDataSource]
+            Source of the data.
+        post_processing_comment : Optional[str]
+            Comment about the postprocessing.
+        source_comment : Optional[str]
+            Comment about the source.
+        xy_abs : Optional[float]
+            Absolute xy error.
+        xy_rel : Optional[float]
+            Relative xy error.
+        z_abs : Optional[float]
+            Absolute z error.
+        z_rel : Optional[float]
+            Relative z error.
+        _error_added : bool
+            Indicates if error information has been added.
+        _raw_data_added : bool
+            Indicates if raw data information has been added.
+        """
+        self.date: Optional[str] = None
+        self.post_processing: Optional["RawDataPostProcessing"] = None
+        self.source: Optional["RawDataSource"] = None
+        self.post_processing_comment: Optional[str] = None
+        self.source_comment: Optional[str] = None
+        self.xy_abs: Optional[float] = None
+        self.xy_rel: Optional[float] = None
+        self.z_abs: Optional[float] = None
+        self.z_rel: Optional[float] = None
+        self._error_added: bool = False
+        self._raw_data_added: bool = False
 
     def add_raw_data_info(
         self,
-        date,
-        post_processing,
-        source,
-        post_processing_comment=None,
-        source_comment=None,
-    ):
-        """add_raw_data_info adds data for the RawData entry.
+        date: str,
+        post_processing: "RawDataPostProcessing",
+        source: "RawDataSource",
+        post_processing_comment: Optional[str] = None,
+        source_comment: Optional[str] = None,
+    ) -> None:
+        """Add raw data information to the DataQuality.
 
         Parameters
         ----------
-            date (str): code of the userdata
-
-            post_processing (RawDataPostProcessing): postprocessing definition
-
-            source (RawDataSource): source of the data
-
-            post_processing_comment (str): comment of the postprocessing
-                Default: None
-
-            source_comment (str): comment of the soure
-                Default: None
+        date : str
+            Date of the data.
+        post_processing : RawDataPostProcessing
+            Postprocessing definition.
+        source : RawDataSource
+            Source of the data.
+        post_processing_comment : str, optional
+            Comment about the postprocessing. Default is None.
+        source_comment : str, optional
+            Comment about the source. Default is None.
         """
 
         self.date = date
@@ -339,18 +428,21 @@ class DataQuality:
         self.source_comment = source_comment
         self._raw_data_added = True
 
-    def add_error(self, xy_abs, xy_rel, z_abs, z_rel):
-        """add_error adds data to the error element.
+    def add_error(
+        self, xy_abs: float, xy_rel: float, z_abs: float, z_rel: float
+    ) -> None:
+        """Add error information to the DataQuality.
 
         Parameters
         ----------
-            xy_abs (float): absolute xy error
-
-            xy_rel (float): relative xy error
-
-            z_abs (float): absolute z error
-
-            z_rel (float): relative z error
+        xy_abs : float
+            Absolute xy error.
+        xy_rel : float
+            Relative xy error.
+        z_abs : float
+            Absolute z error.
+        z_rel : float
+            Relative z error.
         """
         self.xy_abs = xy_abs
         self.xy_rel = xy_rel
@@ -358,7 +450,7 @@ class DataQuality:
         self.z_rel = z_rel
         self._error_added = True
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         if isinstance(other, DataQuality):
             if (
                 self.date == other.date
@@ -375,8 +467,14 @@ class DataQuality:
                 return True
         return False
 
-    def get_element(self):
-        """Returns the elementTree of the Junction."""
+    def get_element(self) -> ET.Element:
+        """Return the ElementTree representation of the DataQuality.
+
+        Returns
+        -------
+        ET.Element
+            The XML ElementTree representation of the DataQuality.
+        """
         element = ET.Element("dataQuality")
         if self._raw_data_added:
             raw_data_attrib = {
